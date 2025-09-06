f6997c1e6981cc6638897d2383e90e1777824fd5
Hadi pd <h7ad3i@gmail.com>
2025-09-06 20:08:30 +0330

    Add fields to skills table

--- database/migrations/2025_09_06_154257_create_skills_table.php
+++ database/migrations/2025_09_06_154257_create_skills_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedTinyInteger('percentage');
+            $table->enum('type', ['primary','success'])->default('primary');
             $table->timestamps();
         });
     }
