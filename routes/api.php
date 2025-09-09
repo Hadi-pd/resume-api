4d5ca2bfe12fdc05ef7a7be083f75619f8bf37e9
Hadi pd <h7ad3i@gmail.com>
2025-09-09 23:35:41 +0330

    Add API resource routes for skills under authenticated middleware

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SkillController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::put('/user/{user}', [AuthController::class, 'updateUser']);
+    
+    Route::apiResource('skills', SkillController::class);
 });
